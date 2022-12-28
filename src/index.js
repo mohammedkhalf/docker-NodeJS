3f0ec85de64f09a02964f72ee34b9b010d90bea2
khalf <khalf>
2022-12-28 04:34:34 +0200

    add mongo service to docker compose

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+// require Modules
 const express = require('express');
+const mongoose = require('mongoose');
 
-
+//init app
 const PORT = process.env.PORT || 4000;   // I passed to container which port that app work on it
-
 const app = express();
 
-app.get('/',(req,res)=> res.send('<h1> Hello bank misr bank khalf ssssss ddd </h1>'));
+//connect db
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = 27017;
+const DB_HOST = 'mongo'   // to connect db inside container use   mongosh -u root -p example
 
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`
+mongoose.connect(URI)
+.then(() => console.log('connect to db....'))
+.catch((err) => console.log('failed to connect to db',err));
+
+
+app.get('/',(req,res)=> res.send('<h1> Hello bank misr bank khalf ssssss ddd </h1>'));
 app.listen (PORT , ()=> console.log(`app is up and running on port: ${PORT}`) );
 
